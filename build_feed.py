68b4fc58d7780cf8248321c7179c9fe97bb65a7a
Tempest790 <113042637+Tempest790@users.noreply.github.com>
2026-02-05 11:36:48 -0600

    Update build_feed.py

--- build_feed.py
+++ build_feed.py
@@ -95,7 +95,7 @@ def build_rss(items):
     </item>""")
 
     return f"""<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>{html.escape(channel_title)}</title>
     <link>{html.escape(channel_link)}</link>
